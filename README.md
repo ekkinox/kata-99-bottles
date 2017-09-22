ee5d21c805d7a57f0f83dc4d5c2834394189956b
Jonathan VUILLEMIN <jonathan.vuillemin@doclerholding.com>
2017-09-22 11:35:10 +0200

    Do kata

---
+++ README.md
@@ -0,0 +1,18 @@
+# Code Kata: 99 Bottles
+
+## Kata Subject
+
+The code kata subject can be found [at this link](https://www.codewars.com/kata/99-bottles-of-beer-1/javascript).
+
+## Usage
+
+Install dependencies
+```
+$ composer install
+```
+
+## Tests
+
+Run tests (coverage in coverage/ folder)
+```
+$ vendor/bin/phpunit
---
+++ src/Singer.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace Ekkinox\KataBottles;
+
+/**
+ * @package Ekkinox\KataBottles
+ */
+class Singer
+{
+	/**
+	 * @return array
+	 */
+	public function sing(): array
+	{
+		$song = [];
+
+		for ($i = 99; $i > 1; $i--)
+		{
+			$song[] = sprintf('%d bottles of beer on the wall, %d bottles of beer.', $i, $i);
+			$song[] = sprintf('Take one down and pass it around, %d bottles of beer on the wall.', $i-1);
+		}
+
+		$song[] = '1 bottle of beer on the wall, 1 bottle of beer.';
+		$song[] = 'Take one down and pass it around, no more bottles of beer on the wall.';
+		$song[] = 'No more bottles of beer on the wall, no more bottles of beer.';
+		$song[] = 'Go to the store and buy some more, 99 bottles of beer on the wall.';
+
+		return $song;
+	}
+}
